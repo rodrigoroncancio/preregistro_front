040de9bba3597839d70b924b357526749c9b1d9e
rodrigo roncancio <rodrigo.roncancio@expansionti.com>
2025-02-23 23:16:08 -0500

    estilos de icons del bottom y pagina inicial en backend

--- src/components/expBars/footer.vue
+++ src/components/expBars/footer.vue
@@ -1,18 +1,34 @@
+<script>
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
+import { faYoutube, faInstagram, faFacebookSquare, faLinkedinIn, faXTwitter } from '@fortawesome/free-brands-svg-icons';
+
+// Agregar los íconos a la librería de FontAwesome
+library.add(faMapMarkerAlt, faYoutube, faInstagram, faFacebookSquare, faLinkedinIn, faXTwitter);
+
+export default {
+  components: {
+    FontAwesomeIcon,
+  },
+};
+</script>
+
 <template>
   <v-container class="px-0">
     <v-row :dense="true">
       <v-col cols="1" class="text-right">
-        <v-icon>mdi-map-marker</v-icon>
+        <font-awesome-icon icon="fa-solid fa-map-marker-alt" class="icon-marker" />
       </v-col>
       <v-col cols="6" class="text-left address">
         Carrera 7 No. 26-20, Edificio Tequendama (Pisos 32 - 33)
       </v-col>
-      <v-col cols="5" class="text-right social mt-1">
-        <v-icon>mdi-youtube</v-icon>
-        <v-icon>mdi-instagram</v-icon>
-        <v-icon>mdi-facebook</v-icon>
-        <v-icon>mdi-linkedin</v-icon>
-        <v-icon>mdi-twitter</v-icon>
+      <v-col cols="5" class="text-right social">
+        <font-awesome-icon icon="fa-brands fa-youtube" />
+        <font-awesome-icon icon="fa-brands fa-facebook-square" />
+        <font-awesome-icon icon="fa-brands fa-instagram" />
+        <font-awesome-icon icon="fa-brands fa-linkedin-in" />
+        <font-awesome-icon icon="fa-brands fa-x-twitter" />
       </v-col>
     </v-row>
   </v-container>
@@ -30,5 +46,28 @@
 
 .v-container {
   min-height: 65px;
+}
+
+/* Ajustes para los íconos */
+.icon-marker {
+  font-size: 22px;
+}
+
+.social {
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  gap: 12px; /* Espaciado entre los iconos */
+}
+
+.social svg {
+  font-size: 22px; /* Tamaño de los íconos */
+  margin: 0 3px; /* Separación lateral */
+  cursor: pointer;
+  transition: transform 0.2s ease-in-out;
+}
+
+.social svg:hover {
+  transform: scale(1.2);
 }
 </style>
