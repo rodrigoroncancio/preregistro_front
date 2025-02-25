3bd31d7d5aa2833effe9af43db2994b0b3e56460
rodrigo roncancio <rodrigo.roncancio@expansionti.com>
2025-02-25 01:28:13 -0500

    se arreglan los botones de validados no validados

--- src/modules/pnis/pages/argeliapersonas.vue
+++ src/modules/pnis/pages/argeliapersonas.vue
@@ -27,6 +27,12 @@
               mdi-account-hard-hat
             </v-icon>
           </template>
+          <template v-slot:item.number_completed="{item}">
+            <v-btn @click="modalValidados(item, 'si')"  variant="flat" color="green" block>{{ item.number_completed }}</v-btn>
+          </template>
+          <template v-slot:item.number_uncompleted="{item}">
+            <v-btn @click="modalValidados(item, 'no')"  variant="flat" color="red" block>{{ item.number_uncompleted }}</v-btn>
+          </template>
         </exp-data-table>
       </v-card-text>
     </v-card>
@@ -83,17 +89,65 @@
     :btnSaveText="$t('modules.core.select')"
   >
     <v-row>
-      <v-col cols="12" v-for="item in itemsDocuments" :key="item.id">
-        <div class="container-documents">
-          <a :href="item.link" target="blank" class="button-documents">{{ item.label }}</a>
-        </div>
+      <v-col cols="12" v-for="(item, index) in itemsDocuments" :key="index">
+        <v-btn @click="downloadDocument(item, 1)" color="green" block><v-icon>mdi-file-download</v-icon> {{ item.label }}</v-btn>
       </v-col>
     </v-row>
   </exp-modal-form>
 
+  <exp-modal-form
+    title="Items validados"
+    :width="850"
+    v-model="formModalValidados"
+    :btnSave="false"
+    @fnSave="clickSelectFormDocuments"
+    :btnSaveText="$t('modules.core.select')"
+    >
+    <v-row>
+      <v-col cols="12">
+        
+      <v-card>
 
+        <v-simple-table class="w-">
+          <thead>
+            <tr>
+              <th class="text-left">Validación</th>
+              <th class="text-left">Observación</th>
+              <th class="text-left">Evidencia</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr
+              v-for="(item, index) in itemsValidados"
+              :key="index"
+              :class="index % 2 === 0 ? 'shadow-md bg-gray-50' : ''"
+            >
+              <td class="py-3 px-4" width="30%">  
+                  {{ itemsValidadosBase.find(option => option.id === item.validationitems_id)?.name || 'Sin nombre' }}
+              </td>
+              <td class="py-3 px-4 observation-cell" width="30%">
+                {{ item.observation || 'Sin observación' }}
+              </td>
+              <td>{{item.user_name}}</td>
+              <td class="py-3 px-4" width="100%">
+                <template v-if="item.attachment">
+                  <v-btn @click="downloadDocument(item, 2)" variant="text" color="green" block>
+                    Ver evidencia
+                  </v-btn>
+                </template>
+                <span v-else>Sin Evidencia</span>
+              </td>
+            </tr>
+          </tbody>
+        </v-simple-table>
+      </v-card>
+         
+
+
+      </v-col>
+    </v-row>          
+  </exp-modal-form>
 </template>
-
 
 
 
@@ -143,14 +197,56 @@ const headers: any[] = [
   { key: "actions", title: t("commons.common.actions"), width: "90px", type: "actions", sortable: false, },
 ];
 const drawRefresh = ref("");
+const validationKey = ref("");
 
 const formModal = ref(false);
 const formModalValidate = ref(false);
 const formModalDocumentos = ref(false);
-
+const formModalValidados = ref(false);
 const itemsValidation = ref<Array<{ id: number; label: string }>>([]);
 const itemsDocuments = ref([]);
-
+const itemsValidados = ref([]);
+const itemsValidadosBase = ref([
+    {
+        "id": 2,
+        "name": "Completitud de los datos",
+    },
+    {
+        "id": 3,
+        "name": "Representante núcleo familiar"
+    },
+    {
+        "id": 4,
+        "name": "Personas del núcleo familiar"
+    },
+    {
+        "id": 5,
+        "name": "Hetarea"
+    },
+    {
+        "id": 6,
+        "name": "Lugar de residencia"
+    },
+    {
+        "id": 7,
+        "name": "Georeferenciación"
+    },
+    {
+        "id": 8,
+        "name": "Usufructo"
+    },
+    {
+        "id": 9,
+        "name": "Acceso a tierras"
+    },
+    {
+        "id": 10,
+        "name": "Arraigo"
+    },
+    {
+        "id": 11,
+        "name": "Técnica"
+    }]);
 const getValidationItems = async () => {
     let loader = uLoading.show({});
     itemsValidation.value = [];
@@ -177,8 +273,46 @@ const getValidationItems = async () => {
     }
   };
 
+const getValidationKey = async () => {
+  try {
+    const response = await axios.get(`/api/1.0/core/media/generatekey/`);
+    validationKey.value = response.data;
+  } catch (error) {
+    console.error("Error fetching validation items:", error);
+  }
+};
 
-onMounted(async () => {});
+const modalValidados = async (item: any, tipo: string='si') => {
+  try {
+    let loader = uLoading.show({});
+    const response = await axios.get(`/api/1.0/core/validationregister/filterbydocumentnumber/${item.identificacion}/2/${tipo}`);
+    itemsValidados.value = response.data;
+    loader.hide();
+    formModalValidados.value = true;
+  } catch (error) {
+    console.error("Error fetching validation items:", error);
+  }
+}
+
+const downloadDocument = (item: any, tipo: number=1) => {
+  const hostServer = import.meta.env.VITE_BASE_MEDIA;
+  const idUser = uAuth.getUserData().id;
+  const enlace = document.createElement('a');
+  console.log(item.attachment);
+  if (tipo == 1) {
+    enlace.href = `${hostServer}/api/1.0/core/media/${validationKey.value}/${idUser}/?ruta=${item.document}`;
+  } else {
+    enlace.href = `${hostServer}/api/1.0/core/media/${validationKey.value}/${idUser}/?ruta=${item.attachment}`;
+  }
+  console.log(enlace.href);
+  document.body.appendChild(enlace);
+  enlace.click();
+  document.body.removeChild(enlace);
+}
+
+onMounted(async () => {
+  getValidationKey();
+});
 
 const fnReloadTable = () => {
   formModal.value = false;
@@ -212,35 +346,22 @@ const clickView = async (item: any) => {
 };
 
 const clickDocuments = (item: any) => {
-  console.log(item);
-  itemsDocuments.value = []; // Reiniciar el array
+  itemsDocuments.value = [];
+  const ltsDocuments = [
+    { field: 'fotodocumentofrente', label: 'Foto de frente' },
+    { field: 'fotodocumentorespaldo', label: 'Foto de respaldo' },
+    { field: 'fototenencia', label: 'Foto de tenencia' }
+  ];
+  ltsDocuments.forEach(async (doc) => {
+    if (doc.field in item && item[doc.field]) {
+      itemsDocuments.value.push({
+        document: item[doc.field],
+        label: doc.label
+      });
+    }
+  });
   formModalDocumentos.value = true;
-
-  if (item.fotodocumentofrente) {
-    itemsDocuments.value.push({
-      id: 1,
-      link: "/api/1.0/core/media/"+ item.id + "/?ruta=" + item.fotodocumentofrente,
-      label: "Foto de frente"
-    });
-  }
-
-  if (item.fotodocumentorespaldo) {
-    itemsDocuments.value.push({
-      id: 2,
-      link: "/api/1.0/core/media/"+ item.id + "/?ruta=" + item.fotodocumentorespaldo,
-      label: "Foto de respaldo"
-    });
-  }
-
-  if (item.fototenencia) {
-    itemsDocuments.value.push({
-      id: 3,
-      link: "/api/1.0/core/media/"+ item.id + "/?ruta=" + item.fototenencia,
-      label: "Foto de tenencia"
-    });
-  }
 };
-
 
 const clickAction = (item: any, action: string) => {
   console.log(item, action);
@@ -270,6 +391,11 @@ const clickSelectFormDocuments = (item: any) => {
 </script>
 
 <style lang="scss">
+.observation-cell {
+  /* Permite que el texto se envuelva y expanda la celda verticalmente */
+  white-space: normal;
+  word-wrap: break-word;
+}
 .container-documents  {
     display: flex;
     flex-direction: column;
