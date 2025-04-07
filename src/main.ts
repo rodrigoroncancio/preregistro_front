5fc0c0461bc36160e60a2f9d308b16075dd57a2f
rodrigo roncancio <rodrigo.roncancio@expansionti.com>
2025-04-06 20:16:54 -0500

    cambios de formularios nuevos e inclusion widget de jorge

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,9 @@ import "vue-loading-overlay/dist/css/index.css";
 import "vue3-toastify/dist/index.css";
 import "@vueup/vue-quill/dist/vue-quill.snow.css";
 import "./assets/scss/app.scss";
+import WidgetCoordinates from "@/components/expSurveyjsWidgets/WidgetCoordinates.vue";
 
 const app = createApp(App);
 registerPlugins(app);
+app.component("survey-coordinates-question", WidgetCoordinates);
 app.mount("#app");
